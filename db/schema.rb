c40748cf6e840425d5c2677eb9861c7f8671bf1e
sakatosi0728 <sakatosi0728@gmail.com>
2020-08-20 19:05:01 +0900

    単体テストコードの実施

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,12 @@ ActiveRecord::Schema.define(version: 2020_08_20_052426) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "nickname", default: "", null: false
+    t.string "family_name", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "family_name_kana", default: "", null: false
+    t.string "first_name_kana", default: "", null: false
+    t.date "birthday", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

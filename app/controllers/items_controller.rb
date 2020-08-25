c0b6659265736e9139130f55101ebe99fb414bf9
sakatosi0728 <sakatosi0728@gmail.com>
2020-08-25 18:03:03 +0900

    ログインしているユーザーだけが、出品ページへ遷移できる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,20 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
 
   def index
+    @items = Item.all
+  end
+
+  def new 
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
   end
 
   private
-
+  def item_params
+    params.require(:item).permit(:image, :name, :explanation, :category_id, :status_id, :shipping_charges_id, :shipping_region_id, :days_until_shipping_id, :price)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,17 @@ class User < ApplicationRecord
     validates :email, format: { with: /\A\S+@\S+\.\S+\z/}
     validates :birthday
 
-    with_options format: { with:/\A[ぁ-んァ-ン一-龥]/}
+    with_options format: { with:/\A[ぁ-んァ-ン一-龥]/} do
       validates :family_name
       validates :first_name
     end
 
-    with_options format: { with:/\A[ァ-ヶー－]+\z/}
+    with_options format: { with:/\A[ァ-ヶー－]+\z/} do
       validates :family_name_kana
       validates :first_name_kana
     end
+
   end
+
+
 end

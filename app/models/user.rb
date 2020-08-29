c61dd4f26f6b4fdb75563e63577bbd1450618b8e
sakatosi0728 <sakatosi0728@gmail.com>
2020-08-29 12:24:04 +0900

    初期エラー解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,12 @@ class User < ApplicationRecord
     validates :email, format: { with: /\A\S+@\S+\.\S+\z/}
     validates :birthday
 
-    with_options format: { with:/\A[ぁ-んァ-ン一-龥]/}
+    with_options format: { with:/\A[ぁ-んァ-ン一-龥]/} do
       validates :family_name
       validates :first_name
     end
 
-    with_options format: { with:/\A[ァ-ヶー－]+\z/}
+    with_options format: { with:/\A[ァ-ヶー－]+\z/} do
       validates :family_name_kana
       validates :first_name_kana
     end
